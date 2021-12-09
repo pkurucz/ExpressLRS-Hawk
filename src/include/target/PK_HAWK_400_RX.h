9482a3058e6fc0b96ee98286c7896099dbc89c1a
PK <pkurucz@gmail.com>
2021-12-08 22:00:02 -0500

    Added GPIO_PIN_PWM_OUTPUTS but commented out

--- src/include/target/PK_HAWK_400_RX.h
+++ src/include/target/PK_HAWK_400_RX.h
@@ -1,5 +1,9 @@
-#define DEVICE_NAME "ELRS 900RX"
-// GPIO pin definitions
+#define DEVICE_NAME "PK HAWK 400 RX"
+
+// GPIO pin definitions - Verified
+//#define GPIO_PIN_PWM_OUTPUTS    {0, 1, 3, 5, 9, 10}
+
+// GPIO pin definitions - TODO
 #define GPIO_PIN_NSS 15
 #define GPIO_PIN_DIO0 4
 #define GPIO_PIN_DIO1 5
